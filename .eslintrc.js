0fd81f1f89d6c1f62822286ee4c24b5204ad37e1
dirtycleanjiejie <dirtycleanjiejie@gmail.com>
2021-07-01 20:58:05 +0800

    。。

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,14 +8,13 @@ module.exports = {
     然而我不想这样子做，我想保留eslint的一些校验规则，
     可以先使用prettier格式化，再使用eslint修复。让prettier兼容eslint。
   */
-  extends: ['plugin:vue/essential', '@vue/standard'],
+  extends: ['plugin:vue/essential', '@vue/standard', 'plugin:prettier/recommended'],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'space-before-function-paren': ['warn', 'always']
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
 // prettier 只是会帮我们格式化代码，并不能够修复所有 eslint 错误，比如定义未使用的变量，prettier 不会自动帮我们删除，要手动删除。
